550b3bc03aece6ad2faa79eaba6e00c1b7537bc5
bfamchon <bfamchon@cutii.io>
2021-04-16 11:36:59 +0200

    modif mdp 3

--- server/utilities/utils.js
+++ server/utilities/utils.js
@@ -1,8 +1,8 @@
 const randRoom = () => {
     var result = '';
-    var hexChars = '0123456789abcdef';
-    for (var i = 0; i < 16; i += 1) {
-        result += hexChars[Math.floor(Math.random() * 6)];
+    var hexChars = '0123456789abcdefghijklmnopqrstuvwxyz';
+    for (var i = 0; i < 6; i += 1) {
+        result += hexChars[Math.floor(Math.random() * 36)];
     }
     return result;
 }
