08b34f96f9dca484b84379212c7143791950d250
bfamchon <bfamchon@cutii.io>
2021-04-16 11:25:31 +0200

    init game 6

--- server/utilities/utils.js
+++ server/utilities/utils.js
@@ -1,8 +1,8 @@
 const randRoom = () => {
     var result = '';
     var hexChars = '0123456789abcdef';
-    for (var i = 0; i < 16; i += 1) {
-        result += hexChars[Math.floor(Math.random() * 16)];
+    for (var i = 0; i < 6; i += 1) {
+        result += hexChars[Math.floor(Math.random() * 6)];
     }
     return result;
 }
